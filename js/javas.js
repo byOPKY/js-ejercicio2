50c69d3b9b7bfa822e8a06ff62b11969ff214968
byOPKY <orrillokarol@gmail.com>
2016-08-25 15:15:44 -0500

    subiendo ejercicio resuelto

---
+++ js/javas.js
@@ -0,0 +1,14 @@
+window.alert("Calcula el promedio");
+var num1 =parseInt(prompt ("Ingrese un numero"));
+var num2 =parseInt(prompt ("Ingrese un numero"));
+var num3 =parseInt(prompt ("Ingrese un numero"));
+var promedio = (num1 + num2 + num3)/3;
+document.write("Total del promedio es " + promedio);
+
+
+
+
+
+
+
+
